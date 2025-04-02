dc37e6debedb8c718222357279e7b255b1abdfed
Akash073-hub <akashramachandrabhatbca24@rvu.edu.in>
2025-04-02 15:41:08 +0530

    program2.py

--- lab7/program2.py
+++ lab7/program2.py
@@ -6,34 +6,26 @@ def temperature_converter():
         print("3. Kelvin to Celsius and Fahrenheit")
         print("4. Exit")
         choice = input("Enter choice (1/2/3/4): ")
-
         if choice == '4':
             print("Exiting the converter.")
             break
-
         try:
             temp = float(input("Enter the temperature: "))
         except ValueError:
             print("Invalid input! Please enter a numeric value.")
             continue
-
         if choice == '1':
             fahrenheit = (temp * 9/5) + 32
             kelvin = temp + 273.15
             print(f"{temp}°C is equal to {fahrenheit}°F and {kelvin}K.")
-        
         elif choice == '2':  
             celsius = (temp - 32) * 5/9
             kelvin = celsius + 273.15
             print(f"{temp}°F is equal to {celsius}°C and {kelvin}K.")
-        
         elif choice == '3':  
             celsius = temp - 273.15
             fahrenheit = (celsius * 9/5) + 32
             print(f"{temp}K is equal to {celsius}°C and {fahrenheit}°F.")
-        
         else:
             print("Invalid choice! Please select a valid option.")
-
-
-mal=temperature_converter()
+temperature_converter()
